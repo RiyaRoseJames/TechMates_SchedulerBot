db6d2d5ba83ae888ec365ca338972d42aca6ecc7
Devalakshmy <82251959+Devalakshmy@users.noreply.github.com>
2022-12-18 03:02:54 +0530

    Added basic bot

--- bot.py
+++ bot.py
@@ -1,33 +1,35 @@
 import discord
+import random
 import os
 from dotenv import load_dotenv
-
-import random
+import asyncio
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 load_dotenv()
-token =os.getenv('token')
-# discord bot token added
-
-# from config import token
-intents=discord.Intents(messages=True)
+# token imported from .env file
+token=os.getenv('token')
+#to enable and disable certain gateway events from triggering and being send
+intents=discord.Intents.all()               
 client = discord.Client(intents=intents) 
-# discord login
-@client.event 
+@client.event         #event handler
 async def on_ready():
-     print('We have logged in as {0.user} '.format(client))
+    # succesfull login of the bot
+    print('We have logged in as {0.user} '.format(client))
      
 @client.event
-async def on_message(message):
+async def on_message(message): 
+    print("message->", message.content)
     if message.author == client.user:
          return
-      
-    
-    async def meet(ctx):
-        await ctx.send(ctx.author)
-        await client.say({}.format(arg))
-        await ctx.channel.send(arg)
-    
+    #spliting and assigning values to variables
+    if message.content.startswith('bot'):
+        message_body = message.content.split(' ')
+        meeting_name = message_body[1]
+        email_id = message_body[2]
+        time_t = message_body[3]
+        msg = f"meeting name -> {meeting_name}\t mail id - {email_id}, time - {time_t}"
+        #bot response
+        await message.channel.send(msg)
     
 
 
 client.run(token)
-
