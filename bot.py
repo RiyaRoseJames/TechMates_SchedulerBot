c26988edd991269d72fa54ee48b1aecb47423706
Devalakshmy <82251959+Devalakshmy@users.noreply.github.com>
2022-12-17 18:08:21 +0530

    arguments added

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 import discord
+import os
+from dotenv import load_dotenv
+
 import random
+load_dotenv()
+token =os.getenv('token')
 # discord bot token added
 
-TOKEN= "MTA1MzU0NjgwMDI5Mzg4Mzk2NA.GCx3mY.dFm-BSNb1PeF9jnRp85YRNhMUeWx_n4LjOK-xE"
+# from config import token
 intents=discord.Intents(messages=True)
 client = discord.Client(intents=intents) 
 # discord login
@@ -14,16 +19,15 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
          return
-     
-    # client message
-    if message.channel.send('/can we conduct meeting at 10'): 
-        # bot reply
-       
-     await message.channel.send("please text your email id")
+      
+    
+    async def meet(ctx):
+        await ctx.send(ctx.author)
+        await client.say({}.format(arg))
+        await ctx.channel.send(arg)
+    
     
 
 
-client.run(TOKEN)
-
-
+client.run(token)
 
